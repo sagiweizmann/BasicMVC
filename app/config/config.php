d842c6a6bbba0d7aca3741e0991f5fa629f63bb3
sagiweizmann <sagiweizmann@gmail.com>
2022-08-31 13:36:59 +0300

    Autoloader and Config init

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+    //DB Params
+    define('DB_HOST', '127.0.0.1');
+    define('DB_USER', 'root');
+    define('DB_PASS', '77021002');
+    define('DB_NAME', 'wmvc');
+    //App Root
+    define('APPROOT',dirname(dirname(__FILE__)));
+    //URL Root
+    define('URLROOT','http://localhost/phpmvcframework/');
+    //Site name
+    define('SITENAME','WeizmannMVC');
